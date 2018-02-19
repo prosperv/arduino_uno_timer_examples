6006070f8f2d168f0683aa68d1140b16c1f2bff3
Prosper Van <vanprosper@gmail.com>
2018-02-19 12:46:47 -0800

    Added Blinky led timer overflow interrupt project

---
+++ BlinkyLedTimerOverflowInterrupt/src/main.cpp
@@ -0,0 +1,34 @@
+/*
+ * Arduino 101: timer and interrupts
+ * 2: Timer1 overflow interrupt example
+ * created by RobotFreak
+ */
+#include <Arduino.h>
+#define ledPin 13
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+
+  // initialize timer1
+  noInterrupts();           // disable all interrupts
+  TCCR1A = 0;
+  TCCR1B = 0;
+
+  TCNT1 = 34286;            // preload timer 65536-16MHz/256/2Hz
+  TCCR1B |= (1 << CS12);    // 256 prescaler
+  TIMSK1 |= (1 << TOIE1);   // enable timer overflow interrupt
+  interrupts();             // enable all interrupts
+}
+// interrupt service routine that wraps a user defined function
+// supplied by attachInterrupt
+ISR(TIMER1_OVF_vect)
+{
+  TCNT1 = 34286;            // preload timer
+  digitalWrite(ledPin, digitalRead(ledPin) ^ 1);
+}
+
+void loop()
+{
+  // your program here...
+}
